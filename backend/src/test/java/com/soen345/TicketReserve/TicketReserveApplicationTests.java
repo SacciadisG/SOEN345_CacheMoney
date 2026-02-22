0e6591d9ce0e84075d41e0d25c3fccb610695008
YehJordan <jordanyeh2003@gmail.com>
2026-02-22 15:06:18 -0500

    initial backend setup + a little hello program to test functionality

---
+++ backend/src/test/java/com/soen345/TicketReserve/TicketReserveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soen345.TicketReserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketReserveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
