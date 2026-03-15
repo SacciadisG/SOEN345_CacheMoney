1709dc07b5849cbb32bddb467c31b75d2765d4fd
Jordan Yeh <jordanyeh2003@gmail.com>
2026-03-15 14:30:54 -0400

    initial backend setup + a little hello program to test functionality (#1)
    * initial backend setup + a little hello program to test functionality
    
    * renaming package name to follow naming conventions
    
    * add script to run backend
    
    ---------
    
    Co-authored-by: Benjamin Liu <liubenjamin8@gmail.com>

---
+++ backend/src/test/java/com/soen345/ticketreserve/TicketReserveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soen345.ticketreserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketReserveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
