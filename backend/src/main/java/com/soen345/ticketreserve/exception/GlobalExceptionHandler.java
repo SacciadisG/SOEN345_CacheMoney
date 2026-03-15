9d6fe993a5dc29c38ca621f971570e9a8c6c8d24
Benjamin Liu <liubenjamin8@gmail.com>
2026-03-15 16:04:29 -0400

    add controller for user

---
+++ backend/src/main/java/com/soen345/ticketreserve/exception/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.soen345.ticketreserve.exception;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BadRequestException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleBadRequest(BadRequestException ex) {
+        return Map.of("error", ex.getMessage());
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleDataIntegrityViolation(DataIntegrityViolationException ex) {
+        return Map.of("error", "Duplicate or invalid data");
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Map<String, String> handleGeneric(Exception ex) {
+        return Map.of("error", "Unexpected server error");
+    }
+}
