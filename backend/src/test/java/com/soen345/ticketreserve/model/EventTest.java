b299d5b54c36bf8ad1a37880b88a9c746f8e6de2
YehJordan <jordanyeh2003@gmail.com>
2026-03-26 20:04:58 -0400

    added more tests

--- backend/src/test/java/com/soen345/ticketreserve/model/EventTest.java
+++ backend/src/test/java/com/soen345/ticketreserve/model/EventTest.java
@@ -11,11 +11,36 @@ public class EventTest {
         event.setEventDate(java.time.LocalDate.of(2024, 10, 15));
         event.setLocation("Montreal");
         event.setEventCapacity(100);
+        event.setStatus("Done");
+        event.setCategory("Tech");
 
         assert(event.getTitle().equals("Spring Meetup"));
         assert(event.getDescription().equals("A meetup to discuss Spring Framework"));
         assert(event.getEventDate().equals(java.time.LocalDate.of(2024, 10, 15)));
         assert(event.getLocation().equals("Montreal"));
         assert(event.getEventCapacity() == 100);
+        assert(event.getStatus().equals("Done"));
+        assert(event.getCategory().equals("Tech"));
+    }
+
+    @Test
+    void testConstructor(){
+        Event event = new Event(
+                "Spring Meetup",
+                 java.time.LocalDate.of(2024, 10, 15),
+                "Montreal",
+                "Tech",
+                100,
+                "A meetup to discuss Spring Framework",
+                "upcoming");
+
+        assert(event.getTitle().equals("Spring Meetup"));
+        assert(event.getDescription().equals("A meetup to discuss Spring Framework"));
+        assert(event.getEventDate().equals(java.time.LocalDate.of(2024, 10, 15)));
+        assert(event.getLocation().equals("Montreal"));
+        assert(event.getEventCapacity() == 100);
+        assert(event.getStatus().equals("upcoming"));
+        assert(event.getCategory().equals("Tech"));
+        assert(event.getOrganizer() == null);
     }
 }
